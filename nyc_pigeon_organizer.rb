61f85b9629210d78acdb31201e517519e6f10ba6
Paul O <paul.orawiec@gmail.com>
2019-11-13 09:52:51 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,5 +5,6 @@ def nyc_pigeon_organizer(data)
   pigeon_list = {}
     #unpack from top down - color, gender, lives --> name
     #assign string value "Theo"=> color key iterate through colors
-
+  data.each do | sub_hash, attribute | #purple, color
+    
 end
