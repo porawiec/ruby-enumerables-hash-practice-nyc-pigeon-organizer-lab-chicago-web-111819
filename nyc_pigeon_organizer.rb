a81283fd00c392836cefa8496e70e0c3acde1e63
Paul O <paul.orawiec@gmail.com>
2019-11-13 10:29:18 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,10 +5,14 @@ def nyc_pigeon_organizer(data)
   pigeon_list = {}
     #unpack from top down - color, gender, lives --> name
     #assign string value "Theo"=> color key iterate through colors
-  data.each do | cgl, attribute_hash | #using pigeon data - purple, color
-    attribute_hash.each do | specs, names_array | #using attribute data - names, purple
+  data.each do | cgl, attribute_hash | #using pigeon data - indiv cgl, cgl
+    attribute_hash.each do | specs, names_array | #using attribute data - specific cgls, names
       names_array.each do | name | #pull out the names
-        pigeon_list[name][cgl] = specs
+        if pigeon_list[name][cgl]
+          pigeon_list[name][cgl].push(specs)
+        else
+          pigeon_list[name][cgl] = specs
+        end
       end
     end
   end
